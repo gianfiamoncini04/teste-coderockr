2436b8ced2a0d339ddafb35a1dec448941cfa93b
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2024-03-03 06:14:03 -0300

    Teste Concluido

--- src/pages/home-infinito.tsx
+++ src/pages/home-infinito.tsx
@@ -1,9 +1,29 @@
 import { Posts } from "../components/posts.tsx";
+import { useState, useEffect } from "react"
 
 export function Home() {
-   return (
-            <>
-              <Posts />
-            </>
-       )
+    const [Infinito, setInfinito] = useState(0)
+
+    useEffect(() => {
+        const Scroll = () => {
+            if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+                setInfinito(a => a + 1)
+            }
+        }
+
+        window.addEventListener('scroll', Scroll);
+
+        return () => {
+          window.removeEventListener('scroll', Scroll);
+        };
+    },[Infinito])
+
+	return (
+		<>
+			<Posts />
+			{[...Array(Infinito)].map((index) => (
+				<Posts key={index} />
+			))}
+		</>
+	);
 }
